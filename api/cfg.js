ead252cd8155661abdaeab19edc1367d44fd19e3
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-02-27 10:58:04 +0700

    Add files via upload

--- api/cfg.js
+++ api/cfg.js
@@ -1,1 +1,8 @@
-
+const CONFIG = {
+FIREBASE_URL:    "https://emote-24906-default-rtdb.asia-southeast1.firebasedatabase.app",
+EMOTE_API:       "https://freefire-emote-api.onrender.com",
+EMOTE_KEY:       "codespecters.com",
+MULTI_API:       "https://ff-multipurpose-api.onrender.com",
+MULTI_KEY:       "codespecter",
+CDN_BASE:        "https://cdn.jsdelivr.net/gh/ShahGCreator/icon@main/PNG/",
+};
